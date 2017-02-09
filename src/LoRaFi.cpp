594412758b9dc8ce0053026cf238602d99616c05
LoRaFi <Lorafi.ceastech@gmail.com>
2017-02-10 04:07:21 +0800

    Add files via upload

---
+++ src/LoRaFi.cpp
@@ -0,0 +1,524 @@
+/*
+ * LoRaFi.cpp version 1.0
+ *
+ *  Created on: Feb 8, 2017
+ *      Authors: KHUDHUR ABDULLAH ALFARHAN and Dr. Ammar Zakaria
+ *      Email: Qudoren@gmail.com , ammarzakaria@unimap.edu.my
+ *
+ *  This library licensed under GNU General Public License (GPL)
+ *  and supported by Centre of Excellence for Advanced Sensor Technology (CEASTech)
+ *  https://ceastech.com/
+ *  Universiti Malaysia Perlis (UniMAP)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#include "LoRaFi.h"
+#include <stdio.h>
+#include <Arduino.h>
+#include <stdlib.h>
+
+
+
+LoRaFi::LoRaFi()
+{
+}
+
+//initialize LoRa module
+void LoRaFi::begin(uint8_t cs, uint8_t rst, uint8_t rx_sw, uint8_t tx_sw, uint8_t dio0)
+{
+	//Initialize the SPI communication 
+ 	SPI.begin();
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	delay(100);
+
+	//Store pins numbers in array to be used in other functions 
+        LoRaPins[0] = cs;
+        LoRaPins[1] = rst;
+        LoRaPins[2] = rx_sw;
+        LoRaPins[3] = tx_sw;
+        LoRaPins[4] = dio0;
+
+	//Set chip select, rest, RX switch and Tx switch pins
+	pinMode(cs, OUTPUT);
+	pinMode(rst, OUTPUT);
+	pinMode(rx_sw, OUTPUT);
+	pinMode(tx_sw, OUTPUT); 
+	pinMode(dio0, INPUT);
+
+	//Rest LoRa chip
+	digitalWrite(rst, HIGH);
+	delay(1);
+	digitalWrite(rst, LOW);
+	delay(10);
+
+	// Set Chip select pin high to stop communication 
+	digitalWrite(cs, HIGH);    
+	delay(10);
+
+	Mode(LORA);
+	delay(10);
+
+delay(100);
+}
+
+
+
+
+//Set LoRa Mode (SLEEP, LORA, STANDBY, TX, RX, RX_CONTINUOUS), FSK/OOK not available in this library
+void LoRaFi::Mode(uint8_t mode)
+{
+
+	if(mode == LORA) 
+	{
+		Write_Register(0x01, SLEEP);	// enter sleep mood to select other modes
+		Write_Register(0x01, LORA);  		//enter user selected mode
+		Write_Register(0x01, LORA | STANDBY); 	//Wake up LoRa module
+	}
+	else { Write_Register(0x01, LORA | mode);}
+
+}
+
+
+
+
+
+
+//Stop LoRa module
+void LoRaFi::end(void)
+{
+  Mode(SLEEP);
+
+  SPI.end();
+
+  // Set Chip select pin high to stop communication
+  digitalWrite(LoRaPins[0], HIGH);     
+  digitalWrite(LoRaPins[2], LOW);
+  digitalWrite(LoRaPins[3], LOW);
+
+}
+
+
+
+//Rest LoRa module, after reset, the setting will return to default and it should be configured
+void LoRaFi::reset(void)
+{
+	//Reset LoRa chip
+	digitalWrite(LoRaPins[1], HIGH);
+	delay(1);
+	digitalWrite(LoRaPins[1], LOW);
+	delay(10);
+}
+
+
+
+//Get RSSI value in dBm
+int16_t LoRaFi::Rssi(void)
+{
+	return -139 + Read_Register(0x1b);
+}
+
+
+
+
+//Get SNR value
+float LoRaFi::SNR(void)
+{
+	return ((int8_t)Read_Register(0x19)) * 0.25;
+}
+
+
+
+
+// Set transmitting power ==> NORMAL_POWER = 13dBm ==> MAX_POWER = 20dBm
+void LoRaFi::TXpower(uint8_t pow)
+{
+  //Enter standby mode to set transmitting power
+  Mode(STANDBY);
+  
+  Write_Register(POWER_REGISTER, pow);
+
+}
+
+
+
+// set channel frequency
+void LoRaFi::ChannelFrequency(long frequency)
+{
+  Mode(STANDBY);
+
+  uint64_t freq = ((uint64_t)frequency << 19) / 32000000;
+
+  Write_Register(0x06, (uint8_t)(freq >> 16));
+  Write_Register(0x07, (uint8_t)(freq >> 8));
+  Write_Register(0x08, (uint8_t)(freq >> 0));
+
+}
+
+
+
+//Set Spreading Factor, the supported Spreading Factor values from 6 to 12, default to 7
+void LoRaFi::SpreadingFactor(uint8_t SF)
+{ 
+    Mode(STANDBY);
+
+//To make the value of SF from 6 to 12
+	if(SF < 6) {SF = 6;}
+	if(SF > 12) {SF = 12;}
+
+    if (SF == 6) 
+    {
+    Write_Register(0x31, 0xc5);
+    Write_Register(0x37, 0x0c);
+    } 
+    else {
+    Write_Register(0x31, 0xc3);
+    Write_Register(0x37, 0x0a);
+         }
+
+  Write_Register(0x1E, (Read_Register(0x1E) & 0x0f) | ((SF << 4) & 0xf0));
+
+}
+
+
+//Set the signal bandwidth of the radio "supported values are 125E3, 250E3, 500E3" default value is 125E3
+void LoRaFi::Bandwidth(long bw)
+{
+  uint8_t set_bw;
+  Mode(STANDBY);
+
+  if (bw <= 125E3) 
+  { set_bw = 0;}
+   
+  else if (bw <= 250E3) 
+  { set_bw = 1;} 
+  
+  else { set_bw = 2;}
+
+  Write_Register(0x1d, (Read_Register(0x1d) & 0x0f) | (set_bw << 6));
+
+}
+
+
+
+//Set the coding rate of LoRa radio "supported values are from 5 to 8"
+void LoRaFi::CodingRate(uint8_t C_Rate)
+{
+  Mode(STANDBY);
+  
+	if (C_Rate < 5) { C_Rate = 5;} 
+
+	else if (C_Rate > 8) { C_Rate = 8;}
+
+  	uint8_t CR = C_Rate - 4;
+
+  	Write_Register(0x1d, (Read_Register(0x1d) & 0xf1) | (CR << 3));
+}
+
+
+
+//Set the preamble length of LoRa radio " supported values from 6 to 65535
+void LoRaFi::PreambleLength(uint16_t length)
+{
+    Mode(STANDBY);
+
+    if (length < 6) {length = 6;}
+
+    Write_Register(0x20, (uint8_t)(length >> 8));
+    Write_Register(0x21, (uint8_t)(length >> 0));
+
+}
+
+
+
+//Set the sync word of LoRa radio it is one-byte "Default is 0x34"
+void LoRaFi::SyncWord(uint8_t sw)
+{
+	Mode(STANDBY);
+
+	Write_Register(0x39, sw);
+}
+
+
+
+
+//Write data to specific register 
+void LoRaFi::Write_Register(uint8_t address, uint8_t data)
+{
+  	// Set Chip select pin low to start communication with LoRa module
+  	digitalWrite(LoRaPins[0], LOW);
+
+  	// Send address with MSB 1 to make it a write command
+  	SPI.transfer(address | 0x80);
+
+  	// Send data
+  	SPI.transfer(data);
+
+  	// Set CS pin high to stop communication
+  	digitalWrite(LoRaPins[0], HIGH);
+}
+
+
+
+//Read data from specific register
+uint8_t LoRaFi::Read_Register(uint8_t address)
+{ 
+  uint8_t RegData;
+
+  	// Set Chip select pin low to start communication with LoRa module
+  	digitalWrite(LoRaPins[0], LOW);
+
+  	// Send register address
+  	SPI.transfer(address);
+
+  	// Send 0x00 to receive the answer from LoRa module 
+  	RegData = SPI.transfer(0x00);
+
+  	// Set Chip select pin high to stop communication with LoRa module
+  	digitalWrite(LoRaPins[0], HIGH);
+	
+	return RegData;
+}
+
+
+
+
+
+//send package
+void LoRaFi::SendPackage(char *Package, uint8_t packageLength)
+{
+  uint16_t i;
+  uint8_t Value;
+
+  	//Switch LoRa to standby mode
+  	Mode(STANDBY);
+  	
+  	//Set payload length
+    Write_Register(0x22, packageLength);
+
+  	//Load FIFO tx pointer
+  	Value = Read_Register(0x0E);	//get FIFO address pointer value
+  	Write_Register(0x0D, Value);	//set SPI interface address pointer in FIFO data buffer.
+
+  	//Transmit Mode
+  	digitalWrite(LoRaPins[3], HIGH); //TX_SW = 1;
+  	digitalWrite(LoRaPins[2], LOW);  //RX_SW = 0;
+
+  	//IRQ mask
+  	Write_Register(0x11, 0x87);
+
+  	//IRQ flag
+  	Write_Register(0x12, 0xFF);
+
+  	//Write payload in to the FIFO
+  	for (i = 0; i < packageLength; i++)
+  	{
+   	 Write_Register(0x80, *Package);
+   	 delay(100);
+   	 Package++;
+  	}
+
+  	//Close TX LoRa
+  	Write_Register(0x01, 0x82);
+  	Write_Register(0x01, 0x83);
+
+  	//Wait until tx done
+  	do
+  	{
+   		Value = Read_Register(0x12);
+
+    	Value = Value & 0x08;
+
+  	} while (Value == 0);
+
+  	delay(1);
+
+  	//stop sending and receiving 
+  	digitalWrite(TX_SW, LOW);  //TX_SW = 0;
+  	digitalWrite(RX_SW, LOW); //RX_SW = 0;
+
+}
+
+
+
+
+//Receive package
+void LoRaFi::ReceivePackage(char *Package, uint8_t packageLength)
+{
+  uint16_t i;
+  uint8_t LoRa_Interrupt;
+  uint8_t PackageLocation;
+  
+  	digitalWrite(LoRaPins[0], HIGH);
+  
+  	//Switch LoRa to standby
+  	Mode(RX_CONTINUOUS);
+  
+    //Load FIFO tx pointer
+    uint8_t value = Read_Register(0x0E);
+    Write_Register(0x0D, value);
+  
+    // Receive Mode
+  	digitalWrite(LoRaPins[3], LOW);		//TX_SW = 0;
+  	digitalWrite(LoRaPins[2], HIGH);	//RX_SW = 1;
+  
+    //IRD mask
+    Write_Register(0x11, 0x87);
+
+    //clear IRQ
+    Write_Register(0x12, 0xFF);
+    
+    while (digitalRead(LoRaPins[4]) == 0)
+    {
+      delay(1);
+    }
+
+  	//Get interrupt register
+  	LoRa_Interrupt = Read_Register(0x12);
+  
+  	//Clear interrupt register
+  	Write_Register(0x12, 0x60);
+
+  	//Get package location
+  	PackageLocation = Read_Register(0x10);
+
+  	//Set SPI pointer to PackageLocation
+  	Write_Register(0x0D, PackageLocation);
+
+  	//Get message
+  	for (i = 0; i < packageLength; i++)
+  	{
+    	*Package = Read_Register(0x00);
+    	Package++;
+  	}
+
+  	//stop sending and receiving 
+  	digitalWrite(TX_SW, LOW);  //TX_SW = 0;
+  	digitalWrite(RX_SW, LOW); //RX_SW = 0;
+
+}
+
+
+
+//handling data for send
+
+//handling constant chat
+void LoRaFi::Send(const char package[])
+{
+	uint8_t length = strlen(package);
+	SendPackage((char *) package, length);
+
+}
+
+
+//handling char
+void LoRaFi::Send(char package)
+{
+	uint8_t length = sizeof(package);
+	SendPackage( &package, length);
+}
+
+//handling unsigned char
+void LoRaFi::Send(unsigned char package)
+{
+	uint8_t length = sizeof(package);
+	SendPackage((char*) &package, length);
+}
+
+
+//handling int
+void LoRaFi::Send(int package)
+{
+	char buffer[10];
+	itoa(package, buffer, 10);
+	SendPackage(buffer, 10);
+}
+
+
+//
+void LoRaFi::Send(unsigned int package)
+{
+	uint8_t length = sizeof(package);
+	char buffer[10];
+	utoa(package, buffer, 10);
+	SendPackage(buffer, 10);
+	Serial.print("size: ");
+	Serial.println(length);
+}
+
+
+//Handling double
+void LoRaFi::Send(double package, uint8_t digit)
+{
+	uint8_t length = sizeof(package);
+
+	char *convertedChar;
+	sprintf(convertedChar, "%f", package);
+	SendPackage(convertedChar, 20);
+	
+}
+
+
+
+//Habdling received data
+
+
+//Handling received int
+int LoRaFi::ReceiveInt(void)
+{
+	char receivedChar[10];
+	//Get received data in char type
+	ReceivePackage(receivedChar,10); 
+	
+	int converted;
+	//convert char data to int
+	sscanf(receivedChar, "%d", &converted);
+	
+	return converted;
+}
+
+
+//Handling received unsigned int
+unsigned int LoRaFi::ReceiveUint(void)
+{
+	char receivedUint[10];
+	//Get received data in char type
+	ReceivePackage(receivedUint,10);
+	
+	//convert char data to unsigned int and return it 
+	return strtoul(receivedUint,NULL,10);
+}
+
+
+//Handling received double
+double LoRaFi::ReceiveDouble(void)
+{
+	char receivedDouble[20];
+	//Get received data in char type
+	ReceivePackage(receivedDouble,20);
+	
+	return atof(receivedDouble);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/LoRaFi.h
@@ -0,0 +1,156 @@
+/*
+ * LoRaFi.h version 1.0
+ *
+ *  Created on: Feb 8, 2017
+ *      Authors: KHUDHUR ABDULLAH ALFARHAN and Dr. Ammar Zakaria
+ *      Email: Qudoren@gmail.com , ammarzakaria@unimap.edu.my
+ *
+ *  This library licensed under GNU General Public License (GPL)
+ *  and supported by Centre of Excellence for Advanced Sensor Technology (CEASTech)
+ *  https://ceastech.com/
+ *  Universiti Malaysia Perlis (UniMAP)
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#ifndef LORAFI_H
+#define LORAFI_H
+
+//Include required libraries
+#include <Arduino.h>
+#include <SPI.h>
+#include <string.h>
+#include <WString.h>
+#include <stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+using namespace std;
+
+//Define modes
+#define SLEEP			0x00
+#define STANDBY			0x01
+#define LORA			0x80
+#define TX				0x03
+#define RX_CONTINUOUS	0x05
+#define RX				0x06
+
+//Define Registers
+#define POWER_REGISTER	0x09
+
+//Define defult pins
+#define CS		10
+#define RST		9
+#define RX_SW	8
+#define TX_SW	7
+#define DIO0	3
+
+//Define other variables 
+#define DEFAULT_SPREADING_FACTOR	7
+#define DEFAULT_BW					125000
+#define DEFAULT_PACKAGE_LENGTH		10
+#define DEFAULT_FREQUENCY			915000000
+#define DEFAUL_C_RATE				5
+#define DEFAULT_PREAMBLE_LENGTH		8
+#define DEFAULT_SW					0x34
+#define MAX_POWER					0x8F
+#define NORMAL_POWER				0x0F
+#define DEFAULT_DIGIT_NO			2
+
+
+class LoRaFi
+{
+   public:
+		LoRaFi();
+		//~LoRaFi();
+		
+        void begin(uint8_t cs = CS, uint8_t rst = RST, uint8_t rx_sw = RX_SW, uint8_t tx_sw = TX_SW, uint8_t dio0 = DIO0);
+        
+		void Write_Register(uint8_t address, uint8_t data);
+		
+		uint8_t Read_Register(uint8_t address);
+		
+		void Mode(uint8_t mode = SLEEP);
+		
+		void TXpower(uint8_t pow = NORMAL_POWER);
+		
+		void SendPackage(char *Package, uint8_t packageLength = DEFAULT_PACKAGE_LENGTH);
+		
+		void ReceivePackage(char *Package, uint8_t packageLength = DEFAULT_PACKAGE_LENGTH);
+		
+		void ChannelFrequency(long frequency = DEFAULT_FREQUENCY);
+		
+		void end(void);
+		
+		void SpreadingFactor(uint8_t SF = DEFAULT_SPREADING_FACTOR);
+		
+		void Bandwidth(long bw = DEFAULT_BW);
+		
+		void CodingRate(uint8_t C_Rate = DEFAUL_C_RATE);
+		
+		void PreambleLength(uint16_t length = DEFAULT_PREAMBLE_LENGTH);
+		
+		void SyncWord(uint8_t sw = DEFAULT_SW);
+
+		void reset(void);
+
+		int16_t Rssi(void);
+
+		float SNR(void);
+
+		//Habdling data for send
+		//void Send(const);
+		//void Send(const String);
+		void Send(const char[]);
+		void Send(char);
+		void Send(unsigned char);
+		void Send(int);
+		void Send(unsigned int);
+		void Send(long);
+		void Send(unsigned long);
+		void Send(double package, uint8_t digit = DEFAULT_DIGIT_NO);
+		
+		//Habdling received data
+		int ReceiveInt(void);			//receive int value
+		unsigned int ReceiveUint(void);	//receive unsigned int value
+		double ReceiveDouble(void);	//receive double value
+	
+	
+private: 
+	uint8_t LoRaPins[5];
+
+	
+};
+
+
+//extern LoRaFi LoRaFi;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://github.com/sandeepmistry/arduino-LoRa/blob/master/API.md
+
+
+
+
+
+
+#endif
